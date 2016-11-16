60287fde67aee6878eb758aa7457915706ba8f40
shahbaz <shahbaz.h96@gmail.com>
2016-11-16 19:33:02 +0530

    Added Info For Govt. Organisation

--- app/src/main/java/testing/example/com/codesprint/GovtOrg.java
+++ app/src/main/java/testing/example/com/codesprint/GovtOrg.java
@@ -51,138 +51,92 @@ public class GovtOrg extends AppCompatActivity {
         final Random generator = new Random();
 
         logos.put("Delhi Metro", getDrawable(R.drawable.metro));
-        infos.put("Delhi Metro", "Capital: Andorra la Vella\n" +
-                "Dialing code: +376\n" +
-                "Currency: Euro\n" +
-                "Population: 79,218 (2013) World Bank\n" +
-                "Official language: Catalan");
+        infos.put("Delhi Metro", "This Railway system is serving Delhi and National Capital Region in India.\n" +
+                "This is the world's 12th largest system in terms of both length and number of stations.\n" +
+                "It was started under leadership of E.Sreedharan");
 
-        logos.put("Airport Authority of India", getDrawable(R.drawable.airport_authority_of_india));
-        infos.put("Airport Authority of India", "Capital: Abu Dhabi\n" +
-                "Dialing code: +971\n" +
-                "Currency: United Arab Emirates dirham\n" +
-                "Population: 9.346 million (2013) World Bank\n" +
-                "President: Khalifa bin Zayed Al Nahyan");
-        logos.put("PETA", getDrawable(R.drawable.peta));
-        infos.put("PETA", "Capital: Kabul\n" +
-                "Currency: Afghan afghani\n" +
-                "President: Ashraf Ghani\n" +
-                "Population: 30.55 million (2013) World Bank\n" +
-                "Official languages: Pashto, Dari");
+        logos.put("Airports Authority of India", getDrawable(R.drawable.airport_authority_of_india));
+        infos.put("Airports Authority of India", "This organisation is under the Ministry of Civil Aviation and is responsible for creating, upgrading, maintaining and managing civil aviation infrastructure in India.\n" +
+                "It provides Air traffic management (ATM) services over Indian airspace and adjoining oceanic areas.\n" +
+                " It also manages a total of 125 Airports, including 18 International Airports, 7 Customs Airports, 78 Domestic Airports and 26 Civil enclaves at Military Airfields");
+
+        logos.put("Archaeological Survey of India", getDrawable(R.drawable.archaeological));
+        infos.put("Archaeological Survey of India", "This is an Indian Government agency attached to the Ministry of Culture that is responsible for Archaeological Research and the conservation and preservation of cultural monuments in the country.\n" +
+                "It was founded in 1861 by Alexander Cunningham who also became its first Director-General.");
 
         logos.put("Indian Army", getDrawable(R.drawable.army));
-        infos.put("Indian Army", "Capital: Tirana\n" +
-                "Dialing code: +355\n" +
-                "Currency: Albanian lek\n" +
-                "President: Bujar Nishani\n" +
-                "Population: 2.774 million (2013) World Bank");
+        infos.put("Indian Army", "This is the land-based branch and the largest component of the Indian Forces.\n" +
+                " The President serves as the Supreme Commander of this organisation.\n" +
+                "The primary mission is to ensure national security and unity, defending the nation from external aggression and threats, and maintaining peace and security within its borders.\n" +
+                "It is the 2nd largest standing army in the world, with 1,325,000 active troops and 1,155,000 reserve troops");
 
 
         logos.put("Make In India", getDrawable(R.drawable.makeinindia));
-        infos.put("Make In India", "Capital: Buenos Aires\n" +
-                "Dialing code: +54\n" +
-                "Currency: Argentine peso\n" +
-                "President: Mauricio Macri\n" +
-                "Population: 41.45 million (2013)");
+        infos.put("Make In India", "It is an initiative launched by the Government of India to encourage multi-national, as well as national companies to manufacture their products in India.\n" +
+                "It was launched by Prime Minister Narendra Modi on 25 September 2014.\n" +
+                "India emerged, after initiation of the programme in 2015 as the top destination globally for foreign direct investment, surpassing the United States of America as well as the People's Republic of China.\n" +
+                "In 2015, India received US$63 billion in FDI");
 
         logos.put("UGC", getDrawable(R.drawable.ugc));
-        infos.put("UGC", "Capital: Canberra\n" +
-                "Dialing code: +61\n" +
-                "Currency: Australian dollar\n" +
-                "Population: 23.13 million (2013) World Bank\n" +
-                "Prime minister: Malcolm Turnbull");
+        infos.put("UGC", "This organisation of India is a statutory body set up by the Indian Union government under Ministry of Human Resource Development, and is charged with coordination, determination and maintenance of standards of higher education.\n" +
+                "It provides recognition to universities in India, and disburses funds to such recognised universities and colleges");
 
-        logos.put("Aadhar", getDrawable(R.drawable.aadhar));
-        infos.put("Armenia", "Capital: Yerevan\n" +
-                "Dialing code: +374\n" +
-                "Continent: Asia\n" +
-                "Population: 2.977 million (2013) World Bank\n" +
-                "Currencies: Noah's Ark silver coins, Armenian dram");
+        logos.put("Aadhaar", getDrawable(R.drawable.aadhar));
+        infos.put("Aadhaar", "The Unique Identification Authority of India (UIDAI) is a central government agency of India.\n" +
+                "Its objective is to collect the biometric and demographic data of residents, store them in a centralised database, and issue a 12-digit unique identity number called _______ to each resident." +
+                "It is considered the world's largest national identification number project");
 
         logos.put("Air India", getDrawable(R.drawable.air));
-        infos.put("Air India", "Capital: Luanda\n" +
-                "Dialing code: +244\n" +
-                "Currency: Angolan kwanza\n" +
-                "President: José Eduardo dos Santos\n" +
-                "Population: 21.47 million (2013) World Bank\n" +
-                "Official language: Portuguese");
-
-        logos.put("Indian Air Force", getDrawable(R.drawable.airforce));
-        infos.put("Indian Air Force", "Capital: Vienna\n" +
-                "Dialing code: +43\n" +
-                "Currency: Euro\n" +
-                "Population: 8.474 million (2013) World Bank\n" +
-                "Official languages: German, Hungarian, Slovenian, Austrian German");
+        infos.put("Air India", "It is the flag carrier airline of India and the third-largest airline in India in terms of passengers carried, after IndiGo and Jet Airways.\n" +
+                "It is owned by a Government of India enterprise, and operates a fleet of Airbus and Boeing aircraft serving 85 domestic and international destinations.\n" +
+                "The airline was founded by J. R. D. Tata as Tata Airlines in 1932");
 
         logos.put("Sarva Shiksha Abhiyan", getDrawable(R.drawable.shiksha));
-        infos.put("Sarva Shiksha Abhiyan", "Capital: Baku\n" +
-                "Dialing code: +994\n" +
-                "Currency: Azerbaijani manat\n" +
-                "Population: 9.417 million (2013) World Bank\n" +
-                "Official language: Azerbaijani");
+        infos.put("Sarva Shiksha Abhiyan", "It is an Indian Government programme aimed at the universalisation of elementary education \"in a time bound manner\", as mandated by the 86th Amendment to the Constitution of India making free and compulsory education to children between the ages of 6 to 14 (estimated to be 205 million children in 2001) a fundamental right.\n" +
+                "The programme was pioneered by former Indian Prime Minister Atal Bihari Vajpayee.");
 
         logos.put("BSNL", getDrawable(R.drawable.bsnl));
-        infos.put("BSNL", "Capital: Bridgetown\n" +
-                "Dialing code: +1\n" +
-                "Currency: Barbadian dollar\n" +
-                "Continent: North America\n" +
-                "Official language: English");
+        infos.put("BSNL", "It is an Indian state-owned telecommunications company headquartered in New Delhi.\n" +
+                "It was incorporated on 15 September 2000 and took over the business of providing of telecom services and network management from the erstwhile Central Government Departments of Telecom Services (DTS) and Telecom Operations (DTO), with effect from 1 October 2000 on a going concern basis.\n" +
+                "It is the largest provider of fixed telephony and broadband services with more than 60% market share and Fifth largest mobile telephony provider in India.");
 
         logos.put("ISRO", getDrawable(R.drawable.isro));
-        infos.put("ISRO", "Capital: Dhaka\n" +
-                "Dialing code: +880\n" +
-                "Currency: Bangladeshi taka\n" +
-                "Prime minister: Sheikh Hasina\n" +
-                "Population: 156.6 million (2013) World Bank\n" +
-                "President: Abdul Hamid");
+        infos.put("ISRO", "It is the only Space Agency in world to reach Mars in its very first attempt and is headed by the Government of Republic of India.\n" +
+                "It is headquartered in the city of Bengaluru. Its vision is to \"harness space technology for national development\", while pursuing space science research and planetary exploration\n" +
+                "Formed in 1969, it superseded the erstwhile Indian National Committee for Space Research (INCOSPAR) established in 1962 by the efforts of independent India's first Prime Minister, Jawaharlal Nehru, and his close aide and scientist Vikram Sarabhai.");
 
-        logos.put("Swatch Bharat", getDrawable(R.drawable.swatch));
-        infos.put("Swatch Bharat", "Capital: City of Brussels\n" +
-                "Dialing code: +32\n" +
-                "Currency: Euro\n" +
-                "Population: 11.2 million (2013) World Bank\n" +
-                "Official languages: French, Dutch, German");
+        logos.put("Swatch Bharat Abhiyan", getDrawable(R.drawable.swatch));
+        infos.put("Swatch Bharat Abhiyan", "It is a national campaign by the Government of India, covering 4,041 statutory cities and towns, to clean the streets, roads and infrastructure of the country.\n" +
+                "The campaign was officially launched on 2 October 2014 at Rajghat, New Delhi, by Prime Minister Narendra Modi. It is India's biggest ever cleanliness drive with 3 million government employees and school and college students of India participating in this event.");
 
         logos.put("Digital India", getDrawable(R.drawable.digital_india_logo_small));
-        infos.put("Digital India", "Capital: Brasília\n" +
-                "Dialing code: +55\n" +
-                "Currency: Brazilian real\n" +
-                "President: Michel Temer\n" +
-                "Population: 200.4 million (2013) World Bank\n" +
-                "Official language: Portuguese");
+        infos.put("Digital India", "It is a campaign launched by the Government of India to ensure that Government services are made available to citizens electronically by improving online infrastructure and by increasing Internet connectivity or by making the country digitally empowered in the field of technology.\n" +
+                "It was launched on 1 July 2015 by Prime Minister Narendra Modi.\n" +
+                "The initiative includes plans to connect rural areas with high-speed internet networks.");
 
         logos.put("Lion Capital", getDrawable(R.drawable.emblem1));
-        infos.put("Lion Capital", "Capital: Thimphu\n" +
-                "King: Jigme Khesar Namgyel Wangchuck\n" +
-                "Prime minister: Tshering Tobgay\n" +
-                "Currencies: Bhutanese ngultrum, Indian rupee\n" +
-                "Official language: Dzongkha");
+        infos.put("Lion Capital", "It is a sculpture of four Asiatic lions standing back to back, on an elaborate base that includes other animals.\n" +
+                "A graphic representation of it was adopted as the official Emblem of India in 1950.\n" +
+                "It was originally placed atop the Aśoka pillar at the important Buddhist site of Sarnath by the Emperor Ashoka, in about 250 BCE.\n" +
+                "The pillar, sometimes called the Aśoka Column, is still in its original location, but the emblem is now in the Sarnath Museum, in the state of Uttar Pradesh, India");
 
         logos.put("Hindustan Petroleum", getDrawable(R.drawable.hindustanpetroleum));
-        infos.put("Canada", "Capital: Ottawa\n" +
-                "Dialing code: +1\n" +
-                "Currency: Canadian dollar\n" +
-                "Population: 35.16 million (2013) World Bank\n" +
-                "Prime minister: Justin Trudeau\n" +
-                "Provinces: Ontario, British Columbia, Québec, Alberta, more");
-        logos.put("PETA", getDrawable(R.drawable.peta));
-        infos.put("PETA", "Dialing code: +41\n" +
-                "Currency: Swiss franc\n" +
-                "Population: 8.081 million (2013) World Bank\n" +
-                "Official languages: French, Romansh, German, Italian\n" +
-                "Colleges and Universities: ETH Zurich, more");
+        infos.put("Hindustan Petroleum", "It is an Indian state-owned oil and natural gas company with its headquarters at Mumbai, Maharashtra.\n" +
+                "It has about 25% marketing share in India among PSUs and a strong marketing infrastructure.\n" +
+                "The Government of India owns 51.11% shares in this and others are distributed amongst financial institutes, public and other investors.\n" +
+                "The company is ranked 367th on the Fortune Global 500 list of the world's biggest corporations as of 2016.");
+
         logos.put("Indian Post", getDrawable(R.drawable.indian_post));
-        infos.put("Indian Post", "Capital: Nicosia\n" +
-                "Dialing code: +357\n" +
-                "Currency: Euro\n" +
-                "Official languages: Turkish, Greek\n" +
-                "Colleges and Universities: University of Cyprus, more");
+        infos.put("Indian Post", "It is a government-operated postal system in India.\n" +
+                "Generally referred to within India as \"the post office\", it is the most widely distributed postal system in the world.\n" +
+                "The postal service is under the Department of Posts, which is part of the Ministry of Communications and Information Technology of the Government of India.");
+
         logos.put("Indian Oil", getDrawable(R.drawable.indianoil));
-        infos.put("Indian Oil", "Capital: Beijing\n" +
-                "Dialing code: +86\n" +
-                "Currency: Renminbi\n" +
-                "Population: 1.357 billion (2013) World Bank\n" +
-                "President: Xi Jinping");
+        infos.put("Indian Oil", "It is India’s Largest Commercial Enterprise, with a net profit of ₹103.99 billion (US$1.5 billion) for the financial year 2015-16.\n" +
+                "Standing true to its corporate vision of being ‘The Energy of India’, This company has been successfully meeting the energy demands of India for more than five decades.\n" +
+                "Being India’s flagship national oil company, this company with a work-force of 33,000 efficient minds is living their vision of becoming ‘a globally admired company’.\n" +
+                "It is the leading Indian Corporate in Fortune’s prestigious ‘Global 500’ listing of world’s largest corporates at 161st position for the year 2016.");
+
         keysAsArray = new ArrayList<String>(logos.keySet());
 
         key = keysAsArray.get(generator.nextInt(keysAsArray.size()));
@@ -194,7 +148,7 @@ public class GovtOrg extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if(!keysAsArray.isEmpty()) {
+                if (!keysAsArray.isEmpty()) {
                     mText.setText("");
                     skipBtn.setEnabled(Boolean.TRUE);
                     tStart = System.currentTimeMillis();
@@ -202,9 +156,7 @@ public class GovtOrg extends AppCompatActivity {
                     logo.setImageDrawable(logos.get(key));
                     mSubmit.setEnabled(Boolean.TRUE);
                     nextBtn.setEnabled(Boolean.FALSE);
-                }
-                else
-                {
+                } else {
                     //Start an activity
                 }
 
@@ -262,7 +214,7 @@ public class GovtOrg extends AppCompatActivity {
                         else
                             score += 5 - penaltyTime;
 
-                    } else if (getCode(key).equalsIgnoreCase(getCode(ans)) || key.contains(ans) && ans.length()>1) {
+                    } else if (getCode(key).equalsIgnoreCase(getCode(ans)) || key.contains(ans) && ans.length() > 1) {
                         Toast.makeText(getBaseContext(), "Almost Correct Answer!", Toast.LENGTH_SHORT).show();
                         keysAsArray.remove(key);
                         mSubmit.setEnabled(Boolean.FALSE);
