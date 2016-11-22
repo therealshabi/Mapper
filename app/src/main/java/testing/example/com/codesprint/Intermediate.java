f5ce2beb0f24619daeb3b320c672397e48d37ffe
shahbaz <shahbaz.h96@gmail.com>
2016-11-23 02:56:36 +0530

    Adding Toggle Button State Preservation

--- app/src/main/java/testing/example/com/codesprint/Intermediate.java
+++ app/src/main/java/testing/example/com/codesprint/Intermediate.java
@@ -1,16 +1,13 @@
 package testing.example.com.codesprint;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ToggleButton;
-
-import static testing.example.com.codesprint.R.drawable.to;
 
 public class Intermediate extends AppCompatActivity {
 
@@ -31,30 +28,36 @@ public class Intermediate extends AppCompatActivity {
         mFlags = (Button) findViewById(R.id.flags);
         mOrg = (Button) findViewById(R.id.govt);
         mAbout = (Button) findViewById(R.id.about);
-        toggle = (ToggleButton) findViewById(R.id.toggleBtn);
+        toggle = (ToggleButton) findViewById(R.id.toggleButton);
+
 
         mFlags.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Intermediate.this,Mapper.class));
+                startActivity(new Intent(Intermediate.this, Mapper.class));
             }
         });
 
         mOrg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Intermediate.this,GovtOrg.class));
+                startActivity(new Intent(Intermediate.this, GovtOrg.class));
             }
         });
 
         mAbout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               startActivity(new Intent(Intermediate.this, About.class));
+                startActivity(new Intent(Intermediate.this, About.class));
             }
         });
 
         sharedPref = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        if (!sharedPref.getBoolean(getString(R.string.toggleState), toggleState)) {
+            toggle.setChecked(Boolean.FALSE);
+        } else {
+            toggle.setChecked(Boolean.TRUE);
+        }
 
         toggle.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,5 +76,14 @@ public class Intermediate extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (toggleState == true)
+            toggle.setChecked(true);
+        else
+            toggle.setChecked(false);
     }
 }
